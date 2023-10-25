48ea4e1cdb72ab35c6f8977cdb42d2571c3c2604
David <75451291+dulkoss@users.noreply.github.com>
2023-10-25 13:16:51 +0200

    optimisation: crawler (almost*) skips all routes that do not have the `static` middleware.

--- src/LaravelStaticServiceProvider.php
+++ src/LaravelStaticServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace Vormkracht10\LaravelStatic;
 
+use Illuminate\Contracts\Http\Kernel;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Vormkracht10\LaravelStatic\Commands\StaticBuildCommand;
 use Vormkracht10\LaravelStatic\Commands\StaticClearCommand;
+use Vormkracht10\LaravelStatic\Middleware\PreventStaticResponseMiddleware;
 
 class LaravelStaticServiceProvider extends PackageServiceProvider
 {
@@ -18,5 +20,14 @@ class LaravelStaticServiceProvider extends PackageServiceProvider
                 StaticClearCommand::class,
                 StaticBuildCommand::class,
             ]);
+    }
+
+    public function packageBooted()
+    {
+        $kernel = $this->app->make(Kernel::class);
+
+        $kernel->prependMiddlewareToGroup('web',
+            PreventStaticResponseMiddleware::class
+        );
     }
 }
---
+++ src/Middleware/PreventStaticResponseMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Vormkracht10\LaravelStatic\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class PreventStaticResponseMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $bypass = config('static.build.bypass_header');
+
+        $key = array_key_first($bypass);
+
+        $value = $bypass[$key];
+
+        if (! $request->header($key) === $value) {
+            return $next($request);
+        }
+
+        $route = Route::current();
+
+        $shouldHandle = in_array(
+            StaticResponse::class,
+            Route::gatherRouteMiddleware($route),
+        );
+
+        if (! $shouldHandle) {
+            return response(status: 200);
+        }
+
+        return $next($request);
+    }
+}
